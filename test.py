803ee81bf3dde3a1920cabaed49183d324cd28e1
Mani <mani.arasteh92@gmail.com>
2024-05-29 20:20:30 +0330

    Deleted unnecessary files

--- test.py
+++
@@ -1,9 +0,0 @@
-import ollama
-
-response = ollama.chat(model='tinyllama', messages=[
-  {
-    'role': 'user',
-    'content': 'How to compress files in Windows',
-  },
-])
-print(response['message']['content'])
